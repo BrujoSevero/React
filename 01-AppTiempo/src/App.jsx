43839f1c8492d5a71fc0b496c92efdefa5f71310
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-11-15 14:24:16 +0100

    PrimeraSubida

---
+++ 01-AppTiempo/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import SmallSquare from './components/SmallSquare'
+
+function App() {
+  return (
+    <section>
+      <SmallSquare />
+      <SmallSquare />
+    </section>
+    
+  )
+}
+
+export default App
