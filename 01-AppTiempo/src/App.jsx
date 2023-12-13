b6aceddee1205871d04f74d9255092820d8cdb9b
Sofía <sofia.hdez2402@gmail.com>
2023-12-13 14:21:32 +0100

    tiempo_modificado

--- 01-AppTiempo/src/App.jsx
+++ 01-AppTiempo/src/App.jsx
@@ -4,14 +4,30 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import SmallSquare from './components/SmallSquare'
+import MediumSquare from './components/MediumSquare'
+import Search from './components/Search'
 
 function App() {
   return (
-    <section>
-      <SmallSquare />
-      <SmallSquare />
-    </section>
-    
+    <div>
+      <header>
+        
+      </header>
+      <div style={{border: '1px solid black'}}>
+        <div>
+          <Search />
+        </div>
+        <div>
+          <MediumSquare />
+        </div>
+        <div>
+          <SmallSquare />
+          <SmallSquare />
+          <SmallSquare />
+          <SmallSquare />
+        </div>
+      </div>
+    </div>
   )
 }
 
